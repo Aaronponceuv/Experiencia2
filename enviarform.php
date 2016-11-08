f481a7f7b71c4ff61a6605c4e7c79d741c2061ca
Pablo Olivares L <pablo.olivares@alumnos.uv.cl>
2016-11-08 12:21:32 -0300

    experiencia 2

---
+++ enviarform.php
@@ -0,0 +1,20 @@
+
+
+
+  
+  
+<html> 
+<head> 
+</head> 
+<body> 
+<h1>Nombre : <?php echo $nombre = $_POST['nombre'];?></h1>
+  <h1>Apellido: <?php echo $apellido = $_POST['apellido'];?><h1>
+  <h1>Sexo: <?php echo $sexo = $_POST['sexo'] ;?><h1>
+  <h1>Fecha: <?php echo $fecha = $_POST['fecha'];?><h1>
+  <h1>Region: <?php echo $region = $_POST['region'];?><h1>
+  <h1>Area de Interes: <?php echo $area = $_POST['area'];?><h1>
+  <h1>Pagina web: <?php echo $pagina = $_POST['pagina'];?><h1>
+  <h1>Correo: <?php echo $correo = $_POST['correo']; ?><h1>
+  <h1>Color: <?php echo $color = $_POST['color'];?><h1>
+</body>
+</html>
